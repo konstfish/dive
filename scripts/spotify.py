53aefa68c5502afebeeab555c6667f7ea1e026e0
David Fischer <david@konst.fish>
2019-03-10 19:09:15 +0100

    update spotify script, fixed spotify widget alignment

--- scripts/spotify.py
+++ scripts/spotify.py
@@ -6,12 +6,15 @@ device_name = "fishpods"
 spot_as='read -r running <<<"$(ps -ef | grep \"MacOS/Spotify\" | grep -v \"grep\" | wc -l)" && test $running != 0 && IFS=\'|\' read -r theArtist theName theState <<<"$(osascript <<<\'tell application "Spotify"\n        set theTrack to current track\n         set theArtist to artist of theTrack\n        set theName to name of theTrack\n        set theState to player state as string\n        return theArtist & "|" & theName & "|" & theState\n     end tell\')" && echo "<span style=\'white-space: nowrap\'>$theArtist - $theName </span>" || echo "<span style=\'white-space: nowrap\'>Not Connected To Spotify</span>"'
 
 out = os.popen("system_profiler SPBluetoothDataType | grep " + device_name + " -A 7").read()
-spot = os.popen(spot_as).read()
+try:
+    spot = os.popen(spot_as).read()
+except:
+    spot = "Not Connected To Spotify"
 
 if("Connected: No" in out):
     print("<div class='container adjust'><div class='bend bend-lft'><div class='icon icon-lft'><img src='dive/ics/14note.png' height='10px'></div><div class='content content-lft'>" + spot + "</div></div></div></div>")
 else:
-    print("<div class='container adjust' style='padding-left: 205px; !important'><div class='bend bend-lft'><div class='icon icon-lft'><img src='dive/ics/14note.png' height='10px'></div><div class='content content-lft'>" + spot + "</div></div></div></div>")
+    print("<div class='container adjust' style='padding-left: 202px; !important'><div class='bend bend-lft'><div class='icon icon-lft'><img src='dive/ics/14note.png' height='10px'></div><div class='content content-lft'>" + spot + "</div></div></div></div>")
 
 '''
 </div><div class='container adjust'><div class='bend bend-lft'><div class='icon icon-lft'><img src='dive/ics/14note.png' height='10px'></div><div class='content content-lft'>" + spot + "</div></div></div></div>
