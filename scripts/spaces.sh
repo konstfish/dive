f842b11efd1a8f2c5dd27157c90752622a79b270
David Fischer <david@konst.fish>
2019-03-13 15:06:34 +0100

    add spaces widget (broken), add sylesheet for widgets

---
+++ scripts/spaces.sh
@@ -0,0 +1,4 @@
+echo "<img src='dive/ics/x.png' width='12px'></div><div class='content'>"
+echo "<img src='dive/ics/x.png' width='12px'></div><div class='content'>"
+echo "<img src='dive/ics/o.png' width='12px'></div><div class='content'>"
+echo "<img src='dive/ics/x.png' width='12px'></div><div class='content'>"
--- widgets/background.coffee
+++ widgets/background.coffee
@@ -3,6 +3,7 @@ refreshFrequency: 100000000000
 render: () ->
     return '''
     <link rel="stylesheet" type="text/css" href="/dive/styles/main.css" />
+    <link rel="stylesheet" type="text/css" href="/dive/styles/widgets.css" />
 
     <div class="background"></div>
     '''
