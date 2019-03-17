f2d387922e0abadf159d964698ec0c62d9b86ebf
Rafael Schreiber <raf.schreiber@gmail.com>
2019-03-17 20:57:18 +0100

    david ist behindert 🗿

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ git clone https://github.com/lavoiesl/osx-cpu-temp
 cd osx-cpu-temp
 make
 mv osx-cpu-temp ../scripts
+cd ..
+brew install ifstat
 
 mkdir -p $HOME/Library/Application\ Support/Übersicht/widgets/dive
 mv * $HOME/Library/Application\ Support/Übersicht/widgets/dive/
