befc77ffb1628b6e4174f852c61a591e6fc7d2cb
David Fischer <david.fishical@gmail.com>
2019-03-20 13:27:02 +0100

    fixed music script, add storage for names

--- scripts/connection.py
+++ scripts/connection.py
@@ -2,10 +2,10 @@
 
 import os, sys
 
-ADAPTER_NAME = "USB 10/100/1000 LAN"
+adapter = "USB 10/100/1000 LAN"
 out = os.popen('dive/scripts/./interfaces').read()
 
-if(ADAPTER_NAME in out):
+if(adapter in out):
     print("<img src='dive/ics/12ethernet.png' width='14px'></div><div class='content'>")
 elif("Wi-Fi" in out):
     #name = os.popen("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'").read()
