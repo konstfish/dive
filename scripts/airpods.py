119ea7f45ab77e00bc7b5daae8c36c0c00faa663
David Fischer <david@konst.fish>
2019-03-22 19:11:20 +0100

    add volume widget, add volume icons, add volume script, fixed airpod/music widget location

--- scripts/airpods.py
+++ scripts/airpods.py
@@ -11,25 +11,18 @@ out = os.popen('dive/scripts/./battfetch').read()
 if("not_connected" not in str(out)):
     case = substring_after(out, "Case: ")
     case = (case[:3])
-    if(str(case) == "0% "):
-        casehtml = "<div class='container small'><div class='bend bend-lft vsmall grn-dark'><div class='icon icon-lft grn-light'><img src='dive/ics/17airpodC.png' height='12'></div><div class='content content-lft'>100%</div></div><span style='padding-right: 65px;'></span></div>"
-    elif(str(case[2]) != "%"):
-        casehtml = "<div class='container small'><div class='bend bend-lft vsmall grn-dark'><div class='icon icon-lft grn-light'><img src='dive/ics/17airpodC.png' height='12'></div><div class='content content-lft'>" + case + "%</div></div><span style='padding-right: 65px;'></span></div>"
-    else:
-        casehtml = "<div class='container small'><div class='bend bend-lft vsmall grn-dark'><div class='icon icon-lft grn-light'><img src='dive/ics/17airpodC.png' height='12'></div><div class='content content-lft'>" + case + "</div></div><span style='padding-right: 65px;'></span></div>"
+    # useless
 
     left = substring_after(out, "Left: ")
     left = (left[:3])
     if(str(left[2]) != "%"):
-        lefthtml = "<div class='container small' style='padding-left: 65px;'><div class='bend bend-lft vsmall grn-dark'><div class='icon icon-lft grn-light'><img src='dive/ics/15airpodL.png' height='12'></div><div class='content content-lft'>" + left + "%</div></div><span style='padding-right: 65px;'></span></div>"
-    else:
-        lefthtml = "<div class='container small' style='padding-left: 65px;'><div class='bend bend-lft vsmall grn-dark'><div class='icon icon-lft grn-light'><img src='dive/ics/15airpodL.png' height='12'></div><div class='content content-lft'>" + left + "</div></div><span style='padding-right: 65px;'></span></div>"
+        left += "%"
+    lefthtml = "<div class='container small' style='padding-left: 75px;'><div class='bend bend-lft vsmall prp-dark'><div class='icon icon-lft prp-light'><img src='dive/ics/15airpodL.png' height='12'></div><div class='content content-lft'>" + left + "</div></div><span style='padding-right: 65px;'></span></div>"
 
     right = substring_after(out, "Right: ")
     right = (right[:3])
     if(str(right[2]) != "%"):
-        righthtml = "<div class='container lmao' style='padding-left: 130px;'><div class='bend bend-lft grn-dark'><div class='icon icon-lft grn-light'><img src='dive/ics/16airpodR.png' height='12'></div><div class='content content-lft' style='width: 20px !important; margin-right: 0px !imporant;'>" + right + "%</div></div></div>"
-    else:
-        righthtml = "<div class='container lmao' style='padding-left: 130px;'><div class='bend bend-lft grn-dark'><div class='icon icon-lft grn-light'><img src='dive/ics/16airpodR.png' height='12'></div><div class='content content-lft' style='width: 20px !important; margin-right: 0px !imporant;'>" + right + "</div></div></div>"
+        right += "%"
+    righthtml = "<div class='container lmao' style='padding-left: 140px;'><div class='bend bend-lft prp-dark'><div class='icon icon-lft prp-light'><img src='dive/ics/16airpodR.png' height='12'></div><div class='content content-lft' style='width: 20px !important; margin-right: 0px !imporant;'>" + right + "</div></div></div>"
 
-    print(casehtml + lefthtml + righthtml)
+    print(lefthtml + righthtml)
--- widgets/up.coffee
+++ widgets/up.coffee
@@ -3,7 +3,7 @@ command: "bash dive/scripts/up"
 refreshFrequency: 5000
 
 render: (output) ->
-  "<div class='container cpu'>
+  "<div class='container'>
       <div class='bend big red-dark'>
         <div class='icon red-light'>
           <img src='dive/ics/8up.png' height='12'>
