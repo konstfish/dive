8caf5cee1514ab0e0357b0275ed8765394ad57f6
David Fischer <david@konst.fish>
2019-03-22 19:19:09 +0100

    ...

--- test.sh
+++
@@ -1,8 +0,0 @@
-echo "[>] Set Up Widget Order? (y/n): \c"
-read order
-if [ $order == "y" ]
-  then
-    ./widget-arranger
-  else
-    echo "[!] you can set this up later by running ./widget-arranger"
-fi
