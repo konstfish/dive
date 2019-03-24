168e21dd886d4ed6496c352e01b54230b8014764
David Fischer <david@konst.fish>
2019-03-24 19:40:02 +0100

    add bar stylesheet, rewrote music&airpod scripts for css variables

--- scripts/airpods.py
+++ scripts/airpods.py
@@ -17,12 +17,12 @@ if("not_connected" not in str(out)):
     left = (left[:3])
     if(str(left[2]) != "%"):
         left += "%"
-    lefthtml = "<div class='container small' style='padding-left: 75px;'><div class='bend bend-lft vsmall prp-dark'><div class='icon icon-lft prp-light'><img src='dive/ics/15airpodL.png' height='12'></div><div class='content content-lft'>" + left + "</div></div><span style='padding-right: 65px;'></span></div>"
+    lefthtml = "<div class='container small' style='padding-left: var(--airpods-padding);'><div class='bend bend-lft vsmall prp-dark'><div class='icon icon-lft prp-light'><img src='dive/ics/15airpodL.png' height='12'></div><div class='content content-lft'>" + left + "</div></div><span style='padding-right: 65px;'></span></div>"
 
     right = substring_after(out, "Right: ")
     right = (right[:3])
     if(str(right[2]) != "%"):
         right += "%"
-    righthtml = "<div class='container lmao' style='padding-left: 140px;'><div class='bend bend-lft prp-dark'><div class='icon icon-lft prp-light'><img src='dive/ics/16airpodR.png' height='12'></div><div class='content content-lft' style='width: 20px !important; margin-right: 0px !imporant;'>" + right + "</div></div></div>"
+    righthtml = "<div class='container lmao' style='padding-left: calc(var(--airpods-padding) + 65px);'><div class='bend bend-lft prp-dark'><div class='icon icon-lft prp-light'><img src='dive/ics/16airpodR.png' height='12'></div><div class='content content-lft' style='width: 20px !important; margin-right: 0px !imporant;'>" + right + "</div></div></div>"
 
     print(lefthtml + righthtml)
--- widgets/background.coffee
+++ widgets/background.coffee
@@ -2,8 +2,7 @@ refreshFrequency: 100000000000
 
 render: () ->
     return '''
-    <link rel="stylesheet" type="text/css" href="/dive/styles/main.css" />
-    <link rel="stylesheet" type="text/css" href="/dive/styles/widgets.css" />
+    <link rel="stylesheet" type="text/css" href="/dive/styles/bar.css" />
 
     <div class="background"></div>
     '''
